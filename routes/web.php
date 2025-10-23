2c65f2944ccdf99641e8b10ad1b17667307913e8
TAS3-maker <apni-tas3-maker-email@domain.com>
2025-10-23 17:27:41 +0530

    fixed the profile pic issue

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('profile_pics/{filename}', function ($filename) {
+    $path = storage_path('app/public/profile_pics/'.$filename);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+    return response()->file($path);
+});
