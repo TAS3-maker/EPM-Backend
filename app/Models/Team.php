76a2d18d1fcf9e3ca2190f652446355153f95806
TAS3-maker <dm@techarchsoftwares.com>
2025-11-14 10:33:42 +0530

    Update Team.php

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -11,9 +11,18 @@ class Team extends Model
 
     protected $fillable = ['name','department_id'];
 
+    // public function users()
+    // {
+    //     return $this->hasMany(User::class, 'team_id');
+    // }
+     // public function users()
+    // {
+    //     return $this->hasMany(User::class, 'team_id');
+    // }
     public function users()
     {
-        return $this->hasMany(User::class, 'team_id');
+        return $this->hasMany(User::class)
+            ->whereJsonContains('team_id', $this->id);
     }
     public function department()
     {
