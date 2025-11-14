653b457b2dc1b63d6a3fae6260c6498b2f7f5154
TAS3-maker <dm@techarchsoftwares.com>
2025-11-14 11:39:24 +0530

    Update TeamResource.php

--- app/Http/Resources/TeamResource.php
+++ app/Http/Resources/TeamResource.php
@@ -20,14 +20,19 @@ class TeamResource extends JsonResource
             'name' => $this->name,
             'created_at' => $this->created_at ? $this->created_at->format('Y-m-d H:i:s') : null,
             'updated_at' => $this->updated_at ? $this->updated_at->format('Y-m-d H:i:s') : null,
-            'department_id'=> $this->department_id ? Department::where('id',$this->department_id)->get(['id','name'])  : null,
+            // 'department_id'=> $this->department_id ? Department::where('id',$this->department_id)->get(['id','name'])  : null,
+            'department' => $this->department ? [
+                'id' => $this->department->id,
+                'name' => $this->department->name
+            ] : null,
             'users' => $this->users->map(function ($user) {
                 return [
                     'id' => $user->id,
                     'name' => $user->name,
                     'email' => $user->email,
                     'phone' => $user->phone_num,
-                    'role' => $user->role ? $user->role->name : null
+                    'role' => $user->role ? $user->role->name : null,
+                    'team_id' => $user->team_id, // array
                 ];
             })
         ];
