98bf71b8ee456cf3d821061d7db81a0efb303134
TAS3-maker <dm@techarchsoftwares.com>
2025-11-27 18:38:35 +0530

    Update in permission - PermissionController.php

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\ApiResponse;
+use App\Http\Resources\PermissionResource;
 use App\Models\Permission;
 use Illuminate\Http\Request;
 
@@ -25,5 +26,29 @@ class PermissionController extends Controller
         } catch (\Exception $e) {
             return ApiResponse::error('Internal Server Error!', ['error' => $e->getMessage()], 500);
         }
+    }
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $permission = Permission::updateOrCreate(
+            ['user_id' => $data['user_id']],
+            $data
+        );
+
+        return ApiResponse::success('User created successfully', new PermissionResource($permission), 201);
+    }
+    public function destroy($id)
+    {
+        $permission = Permission::where('user_id', $id)->first();
+        if (!$permission) {
+            return response()->json(['message' => 'Permissions not found'], 404);
+        }
+
+        $permission->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Permissions deleted successfully'
+        ]);
     }
 }
