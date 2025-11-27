ecef30815e6bbf8b3d4635a27d059efccd340791
TAS3-maker <dm@techarchsoftwares.com>
2025-11-27 17:39:21 +0530

    added permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'dashboard',
+        'employee_management',
+        'roles',
+        'department',
+        'team',
+        'clients',
+        'projects',
+        'assigned_projects_inside_projects_assigned',
+        'unassigned_projects_inside_projects_assigned',
+        'performance_sheets',
+        'pending_sheets_inside_performance_sheets',
+        'manage_sheets_inside_performance_sheets',
+        'unfilled_sheets_inside_performance_sheets',
+        'manage_leaves',
+        'activity_tags',
+        'leaves',
+        'teams',
+        'leave_management',
+        'project_management',
+        'assigned_projects_inside_project_management',
+        'unassigned_projects_inside_project_management',
+        'performance_sheet',
+        'performance_history',
+        'projects_assigned'
+    ];
+
+    // Relationship to User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
