321f1676b7dd38754a99bb4d2ad9525e246fc476
TAS3-maker <dm@techarchsoftwares.com>
2025-11-28 13:34:44 +0530

    added master resources

---
+++ app/Http/Resources/ProjectRelationResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectRelationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'client_id' => $this->client_id,
+            'project_id' => $this->project_id,
+            'communication_id' => $this->communication_id,
+            'source_id' => $this->source_id,
+            'account_id' => $this->account_id,
+            'client' => $this->client ? $this->client->client_name : null,
+            'project' => $this->project ? $this->project->project_name : null,
+            'communication' => $this->communication ? $this->communication->type_name : null,
+            'source' => $this->source ? $this->source->source_name : null,
+            'account' => $this->account ? $this->account->account_name : null,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
