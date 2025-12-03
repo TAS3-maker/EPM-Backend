ecc82a80948aeb688ef5bd8357e69f7cdd941558
TAS3-maker <dm@techarchsoftwares.com>
2025-12-03 18:19:14 +0530

    Update mail in PerformaSheetController.php

--- app/Http/Controllers/PerformaSheetController.php
+++ app/Http/Controllers/PerformaSheetController.php
@@ -130,7 +130,7 @@ public function addPerformaSheets(Request $request)
 
     // Send email (currently commented)
     foreach ($users as $user) {
-         Mail::to($user->email)->send(new EmployeePerformaSheet($sheetsWithDetails, $user, $submitting_user_name));
+         // Mail::to($user->email)->send(new EmployeePerformaSheet($sheetsWithDetails, $user, $submitting_user_name));
     }
 
     return response()->json([
