ea5993d6ef1ad3393a96531fb34d418b2572359e
TAS3-maker <dm@techarchsoftwares.com>
2025-12-08 17:05:11 +0530

    Update email sending logic in PerformaSheetController
    Changed email sending method to send directly to a specific email instead of queuing for each user.

--- app/Http/Controllers/PerformaSheetController.php
+++ app/Http/Controllers/PerformaSheetController.php
@@ -130,12 +130,9 @@ public function addPerformaSheets(Request $request)
     })->get();
 
     // Send email (currently commented)
-    // foreach ($users as $user) {
-    //      Mail::to($user->email)->send(new EmployeePerformaSheet($sheetsWithDetails, $user, $submitting_user_name));
-    // }
-        foreach ($users as $user) {
-            Mail::to($user->email)->queue(new EmployeePerformaSheet($sheetsWithDetails, $user, $submitting_user_name));
-        }
+    foreach ($users as $user) {
+         Mail::to('backend@techarchsoftwares.com')->send(new EmployeePerformaSheet($sheetsWithDetails, $user, $submitting_user_name));
+    }
 
     return response()->json([
         'success' => true,
