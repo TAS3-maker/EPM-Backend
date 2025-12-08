6eae2cc1b632922cbd95b56818bb4dd6270da4d7
TAS3-maker <dm@techarchsoftwares.com>
2025-12-08 17:58:14 +0530

    Remove unused parameters from constructor
    Comment out unused parameters and properties in the EmployeePerformaSheet class.

--- app/Mail/EmployeePerformaSheet.php
+++ app/Mail/EmployeePerformaSheet.php
@@ -18,15 +18,15 @@ class EmployeePerformaSheet extends Mailable
      */
     public $sheets;
     public $user;
-    public $submitting_user_name;
-    public $submitting_user_employee_id;
+    // public $submitting_user_name;
+    // public $submitting_user_employee_id;
 
-    public function __construct($sheets, $user,$submitting_user_name, $submitting_user_employee_id)
+    public function __construct($sheets, $user)
     {
         $this->sheets = $sheets;
         $this->user = $user;
-        $this->submitting_user_name = $submitting_user_name;
-        $this->submitting_user_employee_id = $submitting_user_employee_id;
+        // $this->submitting_user_name = $submitting_user_name;
+        // $this->submitting_user_employee_id = $submitting_user_employee_id;
 
     }
 
