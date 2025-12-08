34d7ea9ca2beb2cd6cf4db74ecd1a38fb129539f
TAS3-maker <dm@techarchsoftwares.com>
2025-12-08 18:04:20 +0530

    Update EmployeePerformaSheet.php

--- app/Mail/EmployeePerformaSheet.php
+++ app/Mail/EmployeePerformaSheet.php
@@ -18,15 +18,11 @@ class EmployeePerformaSheet extends Mailable
      */
     public $sheets;
     public $user;
-    // public $submitting_user_name;
-    // public $submitting_user_employee_id;
 
     public function __construct($sheets, $user)
     {
         $this->sheets = $sheets;
         $this->user = $user;
-        // $this->submitting_user_name = $submitting_user_name;
-        // $this->submitting_user_employee_id = $submitting_user_employee_id;
 
     }
 
