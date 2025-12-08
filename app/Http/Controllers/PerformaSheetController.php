90bb1429df88fd8a93ba7edbcbd981e41d2eb58b
TAS3-maker <dm@techarchsoftwares.com>
2025-12-08 18:03:58 +0530

    Update PerformaSheetController.php

--- app/Http/Controllers/PerformaSheetController.php
+++ app/Http/Controllers/PerformaSheetController.php
@@ -26,9 +26,6 @@ class PerformaSheetController extends Controller
 public function addPerformaSheets(Request $request)
 {
     $submitting_user = auth()->user();
-    
-    // $submitting_user_name = $submitting_user->name;
-    // $submitting_user_employee_id = $submitting_user->employee_id;
 
     try {
         $validatedData = $request->validate([
