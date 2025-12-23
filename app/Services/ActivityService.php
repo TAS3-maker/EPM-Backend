55695d1831afd745973ea66bdb6644bc2b1c3b8f
TAS3-maker <dm@techarchsoftwares.com>
2025-12-23 15:53:46 +0530

    add activity service

---
+++ app/Services/ActivityService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ProjectActivityAndComment;
+use Illuminate\Support\Facades\Auth;
+
+class ActivityService
+{
+    public static function log(array $data)
+    {
+        return ProjectActivityAndComment::create([
+            'project_id'  => $data['project_id'],
+            'user_id'     => $data['user_id'] ?? Auth::id(),
+            'task_id'     => $data['task_id'] ?? null,
+            'type'        => $data['type'],
+            'description' => $data['description'] ?? null,
+            'attachments' => $data['attachments'] ?? null,
+        ]);
+    }
+}
