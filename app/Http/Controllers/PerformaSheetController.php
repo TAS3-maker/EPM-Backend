dc624cd25766d1601b7302463c60b6b5e1cc6d10
TAS3-maker <dm@techarchsoftwares.com>
2026-01-19 14:04:11 +0530

    comment mail function in perfromasheet

--- app/Http/Controllers/PerformaSheetController.php
+++ app/Http/Controllers/PerformaSheetController.php
@@ -745,15 +745,15 @@ class PerformaSheetController extends Controller
         foreach ($sheetsWithDetailsByDate as $date => $sheetsWithDetails) {
             $formattedDate = Carbon::parse($date)->format('d-m-Y');
             foreach ($users as $approver) {
-                Mail::to($approver->email)->queue(
-                    new EmployeePerformaSheet(
-                        $sheetsWithDetails,
-                        $approver,
-                        $submitting_user_name,
-                        $submitting_employee_id,
-                        $formattedDate
-                    )
-                );
+                // Mail::to($approver->email)->queue(
+                //     new EmployeePerformaSheet(
+                //         $sheetsWithDetails,
+                //         $approver,
+                //         $submitting_user_name,
+                //         $submitting_employee_id,
+                //         $formattedDate
+                //     )
+                // );
             }
         }
 
